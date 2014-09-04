b12589609621ebaf2d524dfd66d48fa51b3d11f5
Andrew Camero <drewe927@gmail.com>
2014-09-04 17:01:50 -0500

    Memorize

---
+++ LearnRubyTheHardWay/ex27.rb
@@ -0,0 +1,83 @@
+# Exercise 27: Memorizing Logic
+
+The Truth Terms
+• and
+• or
+• not
+• != (not equal)
+• == (equal)
+• >= (greater-than-equal)
+69
+Learn Ruby The Hard Way, Release 2.0
+• <= (less-than-equal)
+• true
+• false
+
+The Truth Tables
+We will now use these characters to make the truth tables you need to memorize.
+NOT - opposite
+True? - boolean returnes true of false
+not False - true
+True 
+not True - false
+False
+OR
+True?
+True or False - either conition has to be met
+True
+True or True - either conition has to be met
+True
+False or True - either conition has to be met
+True
+False or False - either conition has to be met
+False
+AND
+True?
+True and False - both conditions have to be met
+False
+True and True - both conditions have to be met
+True
+False and True - both conditions have to be met
+False
+False and False - both conditions have to be met
+False
+NOT OR
+True?
+not (True or False) - opposite of the output of (either conition has to be met)
+False
+not (True or True)
+False
+not (False or True)
+False
+not (False or False)
+True
+NOT AND
+True?
+not (True and False) - opposite of the output of (both conitions have to be met)
+True
+not (True and True)
+False
+not (False and True)
+True
+not (False and False)
+True
+!=
+True?
+1 != 0 - 1 is not equal to 0 (1 being true and 0 being false)
+True
+1 != 1
+False
+0 != 1
+True
+0 != 0
+False
+== - comparison to see if something is equal to another
+True?
+1 == 0
+False
+1 == 1
+True
+0 == 1
+False
+0 == 0
+True
