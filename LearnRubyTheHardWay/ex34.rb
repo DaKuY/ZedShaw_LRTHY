a082e394fc6df1db7f3425af0d244b93ed4b6c1b
Andrew Camero <drewe927@gmail.com>
2014-09-15 12:50:11 -0500

    Added logic to say if the animal is the 1st, 2nd, 3rd, etc.. in the array

--- LearnRubyTheHardWay/ex34.rb
+++ LearnRubyTheHardWay/ex34.rb
@@ -16,9 +16,30 @@ puts "The 1st animal is at 0 and is a bear."
 puts "The animal at 0 is the 1st animal and is a bear."
 
 count = 0
+place = '1st'
+
 animals.each_with_index {|animals, count| 
-puts "The animal at #{count} and is a #{animals}."
-puts "The animal at #{count} is the #{count+1}(st/end/rd/th) animal and is a #{animals}."}
+  puts "The animal at #{count} and is a #{animals}."
+  if count == 0
+    puts "The animal at #{count} is the #{place} animal and is a #{animals}."
+  elsif count == 1
+    place = '2nd'
+	puts "The animal at #{count} is the #{place} animal and is a #{animals}."
+  elsif count == 2
+    place = '3rd'
+	puts "The animal at #{count} is the #{place} animal and is a #{animals}."
+  elsif count == 3
+    place = '4th'
+	puts "The animal at #{count} is the #{place} animal and is a #{animals}."
+  elsif count == 4
+    place = '5th'
+	puts "The animal at #{count} is the #{place} animal and is a #{animals}."
+  elsif count == 5
+    place = '6th'
+	puts "The animal at #{count} is the #{place} animal and is a #{animals}."
+  end
+}
+
 
   
 
